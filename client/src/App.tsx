cbc6a64db7912522b9ba473f54661e1d1367f24a
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-08-20 00:27:21 +0200

    Add ResetPasswordPage and enhance Button styles
    Introduces a new ResetPasswordPage component for password reset functionality. Also updates the Button component to include hover and transition effects for improved user experience.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import LoginPage from "./pages/Login";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 import SignupPage from "./pages/Signup";
 
 function App() {
   return (
     <>
       {/* <LoginPage /> */}
-      <SignupPage />
+      {/* <SignupPage /> */}
+      <ResetPasswordPage />
     </>
   );
 }
