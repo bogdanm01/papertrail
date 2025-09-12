7b5c801235ad0138aedf29cd06996ce7bf43cb74
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-09-13 00:05:22 +0200

    Add userTable schema to drizzle db client configuration

--- server/src/data/db.ts
+++ server/src/data/db.ts
@@ -1,7 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 
+import { userTable } from '@/data/schema/user.schema.js';
+
 import env from '../config/env.js';
 
-const db = drizzle(env.DATABASE_URL);
+const db = drizzle(env.DATABASE_URL, { schema: { userTable } });
 
 export default db;
