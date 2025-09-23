a706e0b653f82c2e8db4d5979358debad8dc4bc6
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-09-23 20:25:11 +0200

    Rename middleware function to use verbs

--- server/src/middlewares/bodyValidator.ts
+++ server/src/middlewares/bodyValidator.ts
@@ -5,11 +5,12 @@ import { z } from 'zod';
 
 import type { ApiResponseBody } from '@/lib/interfaces/apiResponseBody.js';
 
-export const validateRequestBody = (schema: ZodObject): RequestHandler => {
+export const validateBody = (schema: ZodObject): RequestHandler => {
   return (req, res, next) => {
     const result = schema.safeParse(req.body);
 
     if (!result.success) {
+      // TODO: Throw custom ValidationError
       return res.status(400).json({ success: false, errors: z.flattenError(result.error) } as ApiResponseBody<never>);
     }
 
--- server/src/routes/auth/auth.routes.ts
+++ server/src/routes/auth/auth.routes.ts
@@ -1,30 +1,30 @@
 import { Router } from 'express';
-import { StatusCodes } from 'http-status-codes';
 import { container } from 'tsyringe';
 
 import { AuthController } from '@/controllers/auth.controller.js';
 import { getAccessGuard } from '@/middlewares/accessGuard.js';
 import { getRefreshGuard } from '@/middlewares/refreshGuard.js';
-import { validateRequestBody } from '@/middlewares/validateRequestBody.js';
+import { validateBody } from '@/middlewares/bodyValidator.js';
 import { SignInSchema } from '@/lib/zod/signInSchema.js';
 import { SignUpSchema } from '@/lib/zod/signUpSchema.js';
 
+// TODO: Add rate limiting
 const getAuthRouter = () => {
   const authRouter = Router();
   const authController = container.resolve(AuthController);
 
-  const accessGuard = getAccessGuard();
-  const refreshGuard = getRefreshGuard();
+  const requireAuth = getAccessGuard();
+  const requireRefresh = getRefreshGuard();
 
-  authRouter.post('/sign-up', validateRequestBody(SignUpSchema), (req, res) => authController.signUp(req, res));
+  authRouter.post('/sign-up', validateBody(SignUpSchema), (req, res) => authController.signUp(req, res));
 
-  authRouter.post('/sign-in', validateRequestBody(SignInSchema), (req, res) => authController.signIn(req, res));
+  authRouter.post('/sign-in', validateBody(SignInSchema), (req, res) => authController.signIn(req, res));
 
-  authRouter.post('/sign-out', accessGuard, (req, res) => authController.signOut(req, res));
+  authRouter.post('/sign-out', requireAuth, (req, res) => authController.signOut(req, res));
 
-  authRouter.post('/refresh', refreshGuard, (req, res) => authController.refresh(req, res));
+  authRouter.get('/me', requireAuth, (req, res) => authController.me(req, res));
 
-  authRouter.get('/me', accessGuard, (req, res) => authController.me(req, res));
+  authRouter.post('/refresh', requireRefresh, (req, res) => authController.refresh(req, res));
 
   return authRouter;
 };
