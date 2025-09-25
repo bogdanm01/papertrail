436cdc8d7cf6c81a2b7cd10e28d0120456372175
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-09-25 15:32:32 +0200

    Install shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       {/* <LoginPage /> */}
-      {/* <SignupPage /> */}
-      <ResetPasswordPage />
+      <SignupPage />
+      {/* <ResetPasswordPage /> */}
     </>
   );
 }
