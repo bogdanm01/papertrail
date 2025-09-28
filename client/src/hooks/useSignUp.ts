213316f85920cfb0cd12c4a9ce1f282e4ccac171
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-09-28 23:58:03 +0200

    Update useSignUp.ts

--- client/src/hooks/useSignUp.ts
+++ client/src/hooks/useSignUp.ts
@@ -1,13 +1,30 @@
 import { useState } from "react";
 import axiosClient from "@/axiosClient";
+import { useNavigate } from "@tanstack/react-router";
 
 export const useSignUp = () => {
-  const [data, setData] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
 
   const signUp = async (payload: { email: string; password: string }) => {
-    const response = await axiosClient.post("/api/v1/sign-up");
-    console.log(response);
+    setIsLoading(true);
+
+    try {
+      const response = await axiosClient.post("/api/v1/auth/sign-up", payload);
+      setData(response.data);
+    } catch (error: any) {
+      if (error.response) {
+        setError(error.response.data);
+      } else if (error.request) {
+        setError(error.request);
+      } else {
+        setError((error as Error).message as string);
+      }
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  return { signUp, data, isLoading, error };
 };
