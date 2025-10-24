a1424aeb43a46a862962abcce8c18089943da79a
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-10-25 00:09:21 +0200

    Refactor token TTL constants and update cookie maxAge

--- server/src/lib/const.ts
+++ server/src/lib/const.ts
@@ -1,9 +1,12 @@
+const ACCESS_TTL_SEC = 10 * 60;
+const REFRESH_TTL_SEC = 10 * 24 * 60 * 60;
+
 const accessCookieOptions = Object.freeze({
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
   sameSite: 'lax',
   path: '/api',
-  maxAge: 0,
+  maxAge: ACCESS_TTL_SEC * 1000, // express excepts ms
 });
 
 const refreshCookieOptions = Object.freeze({
@@ -11,15 +14,15 @@ const refreshCookieOptions = Object.freeze({
   secure: process.env.NODE_ENV === 'production',
   sameSite: 'lax',
   path: '/api/v1/auth/refresh',
-  maxAge: 0,
+  maxAge: REFRESH_TTL_SEC * 1000,
 });
 
 export const authConsts = Object.freeze({
   ACCESS_TOKEN_NAME: 'papertrail_access',
   REFRESH_TOKEN_NAME: 'papertrail_refresh',
   TOKEN_ISSUER: 'papertrail-api',
-  ACCESS_TTL_SEC: 10 * 60,
-  REFRESH_TTL_SEC: 10 * 24 * 60 * 60,
+  ACCESS_TTL_SEC,
+  REFRESH_TTL_SEC,
   ACCESS_COOKIE_OPTIONS: accessCookieOptions,
   REFRESH_COOKIE_OPTIONS: refreshCookieOptions,
 });
