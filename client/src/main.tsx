caa16189b76509cc3e4d5d7ccab7a5b6bc13d1b4
Bogdan Milojević <45717276+bogdanm01@users.noreply.github.com>
2025-10-24 23:11:10 +0200

    Integrate React Query and devtools into app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,8 +4,17 @@ import "./index.css";
 
 import { routeTree } from "./routeTree.gen";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const router = createRouter({ routeTree });
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./lib/queryClient";
+
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
@@ -15,6 +24,9 @@ declare module "@tanstack/react-router" {
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
